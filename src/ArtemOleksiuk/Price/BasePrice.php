6010062856dfe24f34966b9d96b75fc9db20a173
Artem Oleksiuk <a.oleksuk@zoobio.de>
2019-02-08 17:43:01 +0200

    base price

--- src/ArtemOleksiuk/Price/BasePrice.php
+++ src/ArtemOleksiuk/Price/BasePrice.php
@@ -2,14 +2,98 @@
 
 namespace ArtemOleksiuk\Price;
 
+use Exception;
+
 /**
  * Class BasePrice
  * @package ArtemOleksiuk\Price
  */
 abstract class BasePrice
 {
-    protected function generatePrice()
+    /**
+     * @var float
+     */
+    protected $startPrice;
+
+    /**
+     * @var float
+     */
+    private $markup;
+
+    /**
+     * BasePrice constructor.
+     * @param $price
+     * @param $configs
+     * @throws Exception
+     */
+    public function __construct(float $price, array $configs)
+    {
+        $this->validateConfigs($configs);
+
+        $this->setConfigs($configs);
+        $this->setStartPrice($price);
+    }
+
+    /**
+     * @return float
+     */
+    protected function getStartPrice()
+    {
+        return $this->startPrice;
+    }
+
+    protected static function generateFinalPrice()
     {
 
     }
+
+    /**
+     * All configs must be required
+     *
+     * @return array
+     */
+    private function rules()
+    {
+        return [
+            'markup',
+            // ...
+        ];
+    }
+
+    /**
+     * Configs should contain all the necessary fields to avoid the situation of incorrect pricing
+     *
+     * @param $configs
+     * @throws Exception
+     */
+    private function validateConfigs(array $configs)
+    {
+        foreach ($this->rules() as $rule) {
+
+            if (!array_key_exists($rule, $configs)) {
+                throw new Exception('Config ' . $rule . ' must be required!');
+            }
+
+        }
+    }
+
+    /**
+     * @param array $configs
+     */
+    private function setConfigs(array $configs)
+    {
+        foreach($configs as $item) {
+            $this->markup = $item->markup;
+            // ...
+        }
+    }
+
+    /**
+     * @param float $startPrice
+     */
+    private function setStartPrice(float $startPrice)
+    {
+        $this->startPrice = $startPrice;
+    }
+
 }
